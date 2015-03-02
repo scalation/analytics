ee0ee47b9c5323fbf485974591c94a2de9cc51bf
Michael E. Cotterell <mepcotterell@gmail.com>
2015-03-02 18:57:39 -0500

    added test project

---
+++ project/src/main/scala/apps/analytics/ModelClassifier.scala
@@ -0,0 +1,22 @@
+package apps.analytics
+
+import org.semanticweb.HermiT.Reasoner
+import org.semanticweb.owlapi.model.IRI
+import org.semanticweb.owlapi.apibinding.OWLManager
+
+object ModelClassifier
+{
+
+
+} // ModelClassifier
+
+object ModelClassifierTest extends App
+{
+    val iri      = IRI.create ("https://raw.githubusercontent.com/scalation/analytics/master/analytics.owl")
+    val manager  = OWLManager.createOWLOntologyManager ()
+    val ontology = manager.loadOntologyFromOntologyDocument (iri)
+    val reasoner = (new Reasoner.ReasonerFactory()).createReasoner (ontology);
+
+    println (reasoner.isConsistent ())
+
+} // ModelClassifierTest
