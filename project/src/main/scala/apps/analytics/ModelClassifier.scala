0f25a9ce6a0328cd553d11a8f4a5173842c68edd
Michael E. Cotterell <mepcotterell@gmail.com>
2015-03-02 20:28:11 -0500

    added ability to load locally

--- project/src/main/scala/apps/analytics/ModelClassifier.scala
+++ project/src/main/scala/apps/analytics/ModelClassifier.scala
@@ -1,22 +1,35 @@
 package apps.analytics
 
-import org.semanticweb.HermiT.Reasoner
-import org.semanticweb.owlapi.model.IRI
+import java.io.File
+
+import org.semanticweb.HermiT.{ Reasoner => HermiTReasoner }
+import org.semanticweb.owlapi.model.{ IRI, OWLOntology, OWLOntologyManager }
 import org.semanticweb.owlapi.apibinding.OWLManager
 
 object ModelClassifier
 {
+    val remoteIRI = IRI.create ("https://raw.githubusercontent.com/scalation/analytics/master/analytics.owl")
+    val localIRI  = IRI.create ((new File ("../analytics.owl")).toURI ())
 
+    private def load (iri: IRI): Tuple2[OWLOntologyManager, OWLOntology] =
+    {
+        val manager  = OWLManager.createOWLOntologyManager ()
+        val ontology = manager.loadOntologyFromOntologyDocument (iri)
+        (manager, ontology)
+    } // load
+
+    def loadLocal ()  = load (localIRI)
+    def loadRemote () = load (remoteIRI)
 
 } // ModelClassifier
 
 object ModelClassifierTest extends App
 {
-    val iri      = IRI.create ("https://raw.githubusercontent.com/scalation/analytics/master/analytics.owl")
-    val manager  = OWLManager.createOWLOntologyManager ()
-    val ontology = manager.loadOntologyFromOntologyDocument (iri)
-    val reasoner = (new Reasoner.ReasonerFactory()).createReasoner (ontology);
+    val (manager, ontology) = ModelClassifier.loadRemote ()
+    
+    val hreasoner = (new HermiTReasoner.ReasonerFactory()).createReasoner (ontology);
 
-    println ("Ontology is consistent: " + reasoner.isConsistent ())
+    println ("Ontology is consistent?")
+    println (" - HermiT: " + hreasoner.isConsistent ())
 
 } // ModelClassifierTest
