1ace8972c84dc4663392f06cabc6504b1a96e763
Michael E. Cotterell <mepcotterell@gmail.com>
2015-03-02 20:58:49 -0500

    updated sample app

--- project/src/main/scala/apps/analytics/ModelClassifier.scala
+++ project/src/main/scala/apps/analytics/ModelClassifier.scala
@@ -1,31 +1,20 @@
 package apps.analytics
 
-import java.io.File
-
 import org.semanticweb.HermiT.{ Reasoner => HermiTReasoner }
 import org.semanticweb.owlapi.model.{ IRI, OWLOntology, OWLOntologyManager }
 import org.semanticweb.owlapi.apibinding.OWLManager
 
 object ModelClassifier
 {
-    val remoteIRI = IRI.create ("https://raw.githubusercontent.com/scalation/analytics/master/analytics.owl")
-    val localIRI  = IRI.create ((new File ("../analytics.owl")).toURI ())
-
-    private def load (iri: IRI): Tuple2[OWLOntologyManager, OWLOntology] =
-    {
-        val manager  = OWLManager.createOWLOntologyManager ()
-        val ontology = manager.loadOntologyFromOntologyDocument (iri)
-        (manager, ontology)
-    } // load
-
-    def loadLocal ()  = load (localIRI)
-    def loadRemote () = load (remoteIRI)
 
 } // ModelClassifier
 
+/** The `ModelClassifierTest` object contains some tests for the
+ *  `ModelClassifier` class.
+ */ 
 object ModelClassifierTest extends App
 {
-    val (manager, ontology) = ModelClassifier.loadRemote ()
+    val (manager, ontology) = AnalyticsOntology.loadRemote ()
     
     val hreasoner = (new HermiTReasoner.ReasonerFactory()).createReasoner (ontology);
 
