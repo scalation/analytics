84ba0633ad781cb3e831e665cc673fe0110dbdca
Michael E. Cotterell <mepcotterell@gmail.com>
2015-03-03 14:05:10 -0500

    added a test for consistency

---
+++ project/src/test/scala/apps/analytics/AnalyticsTestSuite.scala
@@ -0,0 +1,40 @@
+package apps.analytics
+
+import org.scalatest._
+
+import org.semanticweb.HermiT.{ Reasoner => HermiTReasoner }
+import uk.ac.manchester.cs.jfact.JFactFactory
+
+/**
+ * 
+ */
+class AnalyticsTestSuite extends FunSuite with GivenWhenThen
+{
+
+    /** This returns the shared fixture containing the things
+      * the tests need to do their work.
+      */
+    def fixture = {
+        new {
+            val (manager, ontology) = AnalyticsOntology.loadLocal ()
+            val hreasoner = (new HermiTReasoner.ReasonerFactory()).createReasoner (ontology);
+            val jreasoner = (new JFactFactory()).createReasoner (ontology)
+        } // new
+    } // fixture
+
+    test ("The ontology should be consistent with all supported reasoners")
+    {
+        // test with HermiT
+        Given ("the unalterted ontology")
+        When ("using HermiT")
+        Then ("the ontology should be consistent")
+        assert (fixture.hreasoner.isConsistent ())
+
+        // test with JFact
+        Given ("the unalterted ontology")
+        When ("using JFact")
+        Then ("the ontology should be consistent")
+        assert (fixture.jreasoner.isConsistent ())
+    } // test
+
+} // AnalyticsTestSuite
