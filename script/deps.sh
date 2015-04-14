6e643943d1bec1a0a9c5236171d4bb510d459aa6
Michael E. Cotterell <mepcotterell@gmail.com>
2015-04-14 06:36:10 -0400

    updated dependency script

--- script/deps.sh
+++ script/deps.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-SCALATION_URL=http://cobweb.cs.uga.edu/~jam/scalation_1.1.1.tar.gz
-PWD=$(pwd)
+SCALATION_URL="http://cobweb.cs.uga.edu/~jam/scalation_1.1.1.tar.gz"
+PWD="$(pwd)"
 
 # setup directory
 mkdir -p scalation
 cd scalation
 
 # download and untar scalation
-wget -o scalation.tar.gz "$SCALATION_URL"
-tar zxvf scalation.tar.gz
+wget "$SCALATION_URL"
+tar zxvf *.tar.gz
 
 # find build.sbt
-BUILD_SBT=$(find . -name "build.sbt")
-PROJ_DIR=$(dirname "$BUILD_SBT")
+BUILD_SBT="$(find . -name "build.sbt")"
+PROJ_DIR="$(dirname "$BUILD_SBT")"
 
 # build and publish locally
 cd $PROJ_DIR
-sbt ++$TRAVIS_SCALA_VERSION compile publish-local
+sbt compile publish-local
 
 # remove the directory
 cd $PWD
