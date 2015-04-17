a72a760756c2276abbfc6ff76e1fdad70e2e434c
Mustafa Nural <mvnural@gmail.com>
2015-04-16 23:32:57 -0400

    Corrected Ontology ID's

--- project/src/main/scala/apps/analytics/VariableTypes.scala
+++ project/src/main/scala/apps/analytics/VariableTypes.scala
@@ -23,16 +23,16 @@ object VariableTypes{
     }
 
     //Continuous Variable Types
-    case object Continuous extends VariableType("Continuous", "Continuous Variable Type")
+    case object Continuous extends VariableType("Continuous_Variable_Type", "Continuous Variable Type")
     case object Non_Negative_Continuous extends VariableType("Non_Negative_Continuous_Variable_Type", "Non Negative Continuous Variable Type")
 
     //Discrete Variable Types
-    case object Discrete extends VariableType("Discrete", "Discrete Variable Type")
+    case object Discrete extends VariableType("Discrete_Variable_Type", "Discrete Variable Type")
 
-    case object Categorical extends VariableType("Categorical", "Categorical Variable Type")
-    case object Binary extends VariableType("Binary", "Binary Variable Type")
-    case object Ordinal extends VariableType("Ordinal", "Ordinal Variable Type")
-    case object Integer extends VariableType("Integer", "Integer Variable Type")
-    case object Non_Negative_Integer extends VariableType("Non_Negative_Integer", "Non Negative Integer Variable Type")
+    case object Categorical extends VariableType("Categorical_Variable_Type", "Categorical Variable Type")
+    case object Binary extends VariableType("Binary_Variable_Type", "Binary Variable Type")
+    case object Ordinal extends VariableType("Ordinal_Variable_Type", "Ordinal Variable Type")
+    case object Integer extends VariableType("Integer_Variable_Type", "Integer Variable Type")
+    case object Non_Negative_Integer extends VariableType("Non_Negative_Integer_Variable_Type", "Non Negative Integer Variable Type")
 
 }
