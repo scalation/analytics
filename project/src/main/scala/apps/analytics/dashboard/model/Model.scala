53712036985cd22ff27f60dfe499067059ee7665
Mustafa Nural <mvnural@gmail.com>
2015-07-26 00:35:45 -0400

    Moved to Package apps.analytics.dashboard. Added method to get explanation for suggested model. Added method to get user friendly label for a suggested model. Updated documentation

---
+++ project/src/main/scala/apps/analytics/dashboard/model/Model.scala
@@ -0,0 +1,94 @@
+package apps.analytics.dashboard.model
+
+import apps.analytics.dashboard.AnalyticsOntology
+import org.semanticweb.owl.explanation.api._
+import org.semanticweb.owlapi.model._
+
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Created by mnural on 3/29/15.
+ */
+class Model (val hasRepeatedObservations : Boolean = false){
+
+    val id = "Model" + System.currentTimeMillis() //append current timestamp to create a unique ID for ontology
+
+    //Conceptual Properties
+
+    //List of model types inferred by the reasoner
+    private var modelTypes : mutable.Set[OWLClass] = null
+
+    //Variables
+    val variables = ArrayBuffer[Variable]()
+
+    //The link function if this model is a GZLM model
+    var linkFunction = null
+
+    var residualDistribution = null
+
+    //Runtime/Algorithmic Properties.
+    //Add type once Algorithm class is created.
+    val algorithms = ArrayBuffer()
+
+    //The data properties
+    //val dataset : MatrixD = null
+    //val responseColumn = null
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** This method returns suitable model types from the ontology for the current
+      * state of this conceptual model
+      * @return suggested models from the analytics ontology for this object
+      */
+    def getModelTypes = {
+        val ontology = AnalyticsOntology.ontology
+        modelTypes = ontology.retrieveTypes(this)
+        modelTypes
+    }
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** Gets user friendly label for the given OWLClass
+      * @param ontologyModel The model class
+      * @return label from the ontology if one exists, the OWLClass name otherwise
+      */
+
+    def getLabel(ontologyModel: OWLClass): String = {
+        val ontology = AnalyticsOntology.ontology
+        for (annotation : OWLAnnotation <- ontologyModel.getAnnotations(ontology.ontology, ontology.factory.getRDFSLabel)) {
+            if (annotation.getValue .isInstanceOf[OWLLiteral]) {
+                val value: OWLLiteral = annotation.getValue().asInstanceOf[OWLLiteral];
+                if (value.getLiteral() != null){
+                    return value.getLiteral
+                }
+            }
+        }
+        return ontologyModel.getIRI.getRemainder.toString
+    }
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** Retrieves possible explanations for the given suggested model.
+      * @param suggestedModel The suggested model class
+      * @return The set of possible explanations for this suggestion
+      */
+
+    def getExplanation(suggestedModel: OWLClass) : Set[Explanation[OWLAxiom]] = {
+      val ontology = AnalyticsOntology.ontology
+      val modelIndividual = ontology.sfProvider.getEntity("analytics:" + this.id).asOWLNamedIndividual()
+      val entailment : OWLAxiom = ontology.factory.getOWLClassAssertionAxiom(suggestedModel, modelIndividual)
+
+      // Create the explanation generator factory which uses reasoners provided by the specified
+      // reasoner factory
+      val genFac: ExplanationGeneratorFactory[OWLAxiom] = ExplanationManager.createExplanationGeneratorFactory(ontology.reasonerFactory);
+
+      // Now create the actual explanation generator for our ontology
+      val gen: ExplanationGenerator[OWLAxiom] = genFac.createExplanationGenerator(ontology.ontology);
+
+      // Get our explanations.  Ask for a maximum of 1
+      val expl = gen.getExplanations(entailment, 1);
+      println(expl)
+      expl.asScala.toSet
+    }
+
+}
