1b69af858f3c4e7c63bc242839b7ba5980c9dcb4
Mustafa Nural <mvnural@gmail.com>
2015-08-22 16:44:43 -0400

    Infer Variable Type from dataset. Don't create table before dataset is loaded.

--- project/src/main/scala/apps/analytics/dashboard/ui/DataTab.scala
+++ project/src/main/scala/apps/analytics/dashboard/ui/DataTab.scala
@@ -1,8 +1,7 @@
 package apps.analytics.dashboard.ui
 
+import java.io.File
 import java.lang.Boolean
-import scala.collection.JavaConverters._
-
 import javafx.beans.property.{ObjectProperty, SimpleObjectProperty}
 import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.collections.{FXCollections, ObservableList}
@@ -14,19 +13,22 @@ import javafx.util.converter.DefaultStringConverter
 import javafx.util.{Callback, StringConverter}
 
 import apps.analytics.dashboard.model.VariableTypes.VariableType
-import apps.analytics.dashboard.model.{Variable, VariableTypes}
+import apps.analytics.dashboard.model.{Model, Variable, VariableTypes}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.io.Source
 
 /**
  * Created by mnural on 8/16/15.
  */
 class DataTab(title : String = "Dataset") extends Tab {
+
   setText(title)
 
-  val table = new TableView[Variable]();
+  var table : TableView[Variable] = null //new TableView[Variable]();
 
-  setContent(table)
-
-  table.setEditable(true);
+  //setContent(table)
 
   val labelColumn = new TableColumn[Variable,String]("Variable");
   labelColumn.setCellValueFactory(new PropertyValueFactory[Variable, String]("fxLabel"))
@@ -58,10 +60,83 @@ class DataTab(title : String = "Dataset") extends Tab {
     }
   })
 
-  table.getColumns().addAll(labelColumn,isResponseColumn, variableTypeColumn)
+  def init(file: File, delimiter: String, model : Model, hasHeaders : Boolean = true) : Unit = {
+    val stream = Source.fromURI(file.toURI)
+    val headers= stream.getLines().next().split(delimiter)
+    //
+    ////    val headers = headerLine.split(delimField.getText())
+        //variables = FXCollections.observableArrayList[Variable]()
+    if (hasHeaders) {
+      headers.foreach(label => model.variables += new Variable(label))
+      val valueList = new Array[Set[String]](headers.length).map(m => mutable.SortedSet[String]())
 
+      stream.getLines().foreach(
+        line => {
+          val values = line.split(delimiter)
+          values.indices.foreach(i => valueList(i) += values(i))
+        }
+      )
+
+      valueList.indices.foreach(i => model.variables(i).setFxVariableType(inferVariableType(valueList(i))))
+
+    } else {
+      var counter = 1
+      for (header <- headers) {
+        model.variables += new Variable("Variable" + counter)
+        counter += 1
+      }
+    }
+    stream.close()
+
+
+    table = new TableView[Variable]()
+    table.getColumns().addAll(labelColumn, isResponseColumn, variableTypeColumn)
+    table.setEditable(true)
+    table.setItems(FXCollections.observableArrayList[Variable](model.variables.asJava))
+    setContent(table)
+
+  }
+
+  def inferVariableType(valueList : mutable.Set[String]): VariableType = {
+    valueList.size match {
+      case 1 =>
+        //TODO HANDLE THIS CASE
+        println("CONSTANT")
+        return VariableTypes.Discrete
+      case 2 =>
+        return VariableTypes.Binary
+      case it if 3 until 10 contains it =>
+        return VariableTypes.Categorical
+      case _ => {
+        try{
+          val doubleList = valueList.map(_.toDouble)
+          if(doubleList.count(d => d.toInt == d) == doubleList.size){
+            if(doubleList.count(_ >= 0) == doubleList.size){
+              return VariableTypes.Non_Negative_Integer
+            }else{
+              return VariableTypes.Integer
+            }
+          } else {
+            if(doubleList.count(_ >= 0) == doubleList.size){
+              return VariableTypes.Non_Negative_Continuous
+            }
+          }
+        } catch  {
+          case e : NumberFormatException => { //Can not be cast to a number.
+            //HOW TO HANDLE THIS?
+            //return null
+            return VariableTypes.Continuous
+          }
+        }
+
+        return VariableTypes.Continuous //Default variable type
+      }
+    }
+  }
 
 }
+
+
 
 class ComboBoxCell extends TableCell[Variable, VariableType]{
 
